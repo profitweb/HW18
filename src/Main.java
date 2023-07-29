22ceefedd859efc23fe90e75e99f6821ed2e5c02
profitweb <chezsimo@yandex.ru>
2023-07-29 17:14:24 +0300

    extends of class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Dog dog = new Dog(5.5, 4,"Melice");
+        dog.move();
+        dog.walk();
+
+        Bird bird = new Bird(2.2, 1, "Cannard");
+        bird.move();
+        bird.walk();
         //todo Реализовать класс Утка и Собака по описанным в ТЗ правилам.
     }
 }
